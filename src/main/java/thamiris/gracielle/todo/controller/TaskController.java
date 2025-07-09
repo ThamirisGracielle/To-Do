fe73785d934ed6607d8b38b2a41c9d44d8ca39a6
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-09 07:21:11 -0300

    feat: Criacao das rotas listall, listById e ListStatus

--- src/main/java/thamiris/gracielle/todo/controller/TaskController.java
+++ src/main/java/thamiris/gracielle/todo/controller/TaskController.java
@@ -1,12 +1,12 @@
 package thamiris.gracielle.todo.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import thamiris.gracielle.todo.DTO.NewTaskDto;
 import thamiris.gracielle.todo.model.Task;
 import thamiris.gracielle.todo.service.TaskService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefa")
@@ -24,5 +24,17 @@ public class TaskController {
     public Task createTask(@RequestBody NewTaskDto newTaskDto){
         return  taskService.create(newTaskDto);
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getById(@PathVariable Long id){
+        Task task = taskService.listById(id);
+        return  ResponseEntity.ok(task);
+    }
+
+    @GetMapping("/{status}")
+    public ResponseEntity<List<Task>> getByStatus(@PathVariable Task.Status status){
+        List<Task> tasks = taskService.listStatus(status);
+        return ResponseEntity.ok(tasks);
     }
 }
