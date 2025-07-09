51d7eadaa3a12e429846546bdfcae63c4e91c6c4
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-09 07:21:33 -0300

    feat: Criacao dos serviços listall, listById e ListStatus

--- src/main/java/thamiris/gracielle/todo/service/TaskService.java
+++ src/main/java/thamiris/gracielle/todo/service/TaskService.java
@@ -2,17 +2,24 @@ package thamiris.gracielle.todo.service;
 
 import org.springframework.stereotype.Service;
 import thamiris.gracielle.todo.DTO.NewTaskDto;
-import thamiris.gracielle.todo.DTO.TaskDto;
 import thamiris.gracielle.todo.model.Task;
 
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
 
     private final AtomicLong contadorId = new AtomicLong(1);
+    private final Map<Long, Task> listTask = new ConcurrentHashMap<>();
 
 
     public Task create (NewTaskDto newTaskDto){
@@ -23,8 +30,29 @@ public class TaskService {
         newTask.setDescription(newTaskDto.getDescription());
         newTask.setDateCreation(LocalDateTime.now());
         newTask.setStatus(Task.Status.PENDENTE);
+        listTask.put(newTask.getId(),newTask);
 
         return newTask;
 
+    }
+
+    public List<Task> listALL(){
+        return new ArrayList<>(listTask.values());
+    }
+
+    public Task listById(Long id){
+        return listALL()
+                        .stream()
+                        .filter(t -> t.getId().equals(id))
+                        .findFirst()
+                        .orElseThrow(() -> new RuntimeException("Id não encontrado"));
+
+    }
+
+    public List<Task> listStatus(Task.Status status) {
+        return listALL()
+                .stream()
+                .filter(t -> t.getStatus().equals(status))
+                .collect(Collectors.toList());
     }
 }
