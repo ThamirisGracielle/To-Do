9552dd14e0c4d4187e559c5de2c8f03895ed8f9b
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-09 02:27:03 -0300

    Commit inicial

---
+++ src/test/java/thamiris/gracielle/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package thamiris.gracielle.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
