e13c2b1f9202762ed1b29723edaf08cba8278da6
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-09 02:44:24 -0300

    feat: Criacao da classe Task do pacote Model

---
+++ src/main/java/thamiris/gracielle/todo/model/Task.java
@@ -0,0 +1,26 @@
+package thamiris.gracielle.todo.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Task {
+
+    private int id;
+    private String description;
+    private LocalDateTime dateCreation;
+    private Status status;
+
+
+    public enum Status{
+
+        COMPLETED,
+        PENDING,
+
+    }
+}
