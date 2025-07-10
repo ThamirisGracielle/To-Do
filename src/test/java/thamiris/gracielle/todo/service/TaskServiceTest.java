d6b950a27516b30f8ec1612d275c4d504e65757b
ThamirisGracielle <thamirisgraciellemoreira@gmail.com>
2025-07-10 00:36:49 -0300

    chore: cobre cenário de erro no método create(

---
+++ src/test/java/thamiris/gracielle/todo/service/TaskServiceTest.java
@@ -0,0 +1,33 @@
+package thamiris.gracielle.todo.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import thamiris.gracielle.todo.DTO.NewTaskDto;
+import thamiris.gracielle.todo.model.Task;
+
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceTest {
+
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp(){
+        taskService = new TaskService();
+    }
+
+    @Test
+    void createTaskSuccessfully(){
+        NewTaskDto dto = new NewTaskDto("Estudar", "Revisar Testes unitários");
+        Task task = taskService.create(dto);
+
+        Assertions.assertNotNull(task.getId());
+        Assertions.assertEquals("Estudar",task.getTitle());
+        Assertions.assertEquals("Revisar Testes unitários", task.getDescription());
+
+    }
+
+}
